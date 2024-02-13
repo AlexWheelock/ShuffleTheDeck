3c6f813245a38f8b3be80884987c5c5df1d7d663
AlexWheelock <framedabyss8@gmail.com>
2024-02-13 08:16:59 -0700

    annotated

--- ShuffleTheDeck/ShuffleTheDeck.vb
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -15,43 +15,58 @@ Module ShuffleTheDeck
         Dim tracker(13, 4) As Boolean
         Dim cardsDrawn As Integer = 0
         Dim message As String = ("Welcome to Draw A Card!" & vbNewLine _
-                          & "Press " & Chr(34) & "D" & Chr(34) & " to draw a card, " & Chr(34) & "N" & Chr(34) & " to start a new game, or " & Chr(34) & "Q" & Chr(34) & " to quit.")
-
+                          & "Press " & Chr(34) & "Enter" & Chr(34) & " to draw a card, " & Chr(34) & "N" & Chr(34) & " to start a new game, or " & Chr(34) & "Q" & Chr(34) & " to quit.")
 
         StartAGame(tracker)
 
         Do
+            'Displays the game and puts together what the player sees
             Display(tracker)
             Console.WriteLine()
             Console.WriteLine(message)
             userInput = Console.ReadLine()
+            'user input is taken, any button draws a card except q and n
+            'q quits the game, n starts a new one
             If userInput = "q" Then
+                'takes user out of loop and displays message
                 Console.WriteLine()
                 Console.WriteLine("Thanks for playing!" & vbNewLine _
                               & "Have a great day!")
                 Console.Read()
                 Exit Sub
             ElseIf userInput = "n" Then
+                'resets the cardsDrawn variable to zero and goes to the StartAGame sub to reset the array
                 StartAGame(tracker)
                 cardsDrawn = 0
             Else
+
+                'logic to determine if all the cards have been drawn
+                'if not, then it will pick a card by going to the PickACard sub and add one to the number of cards drawn.
                 If cardsDrawn >= 52 Then
                     message = ("All cards have been drawn..." & vbNewLine _
                         & "Press " & Chr(34) & "N" & Chr(34) & " to start a new game, or " & Chr(34) & "Q" & Chr(34) & " to quit.")
                 Else
                     PickACard(tracker)
                     cardsDrawn += 1
-                    message = "Press " & Chr(34) & "D" & Chr(34) & " to draw a card, " & Chr(34) & "N" & Chr(34) & " to start a new game, or " & Chr(34) & "Q" & Chr(34) & " to quit."
+                    message = "Press " & Chr(34) & "Enter" & Chr(34) & " to draw a card, " & Chr(34) & "N" & Chr(34) & " to start a new game, or " & Chr(34) & "Q" & Chr(34) & " to quit."
                 End If
             End If
         Loop Until userInput = "q"
 
     End Sub
-
+    ''' <summary>
+    ''' Starts a new game. Redims the tracker array to clear it and returns to the main sub.
+    ''' </summary>
+    ''' <param name="tracker"></param>
     Sub StartAGame(ByRef tracker(,) As Boolean)
         ReDim tracker(13, 4)
     End Sub
 
+    ''' <summary>
+    ''' Picks a random card by picking a random number for each suit and value of card. 
+    ''' Repeats the process until the combination is new.
+    ''' </summary>
+    ''' <param name="tracker"></param>
     Sub PickACard(ByRef tracker(,) As Boolean)
         Dim currentSuit As Integer
         Dim currentValue As Integer
@@ -61,10 +76,16 @@ Module ShuffleTheDeck
             currentValue = RandomNumberTo(13)
         Loop While tracker(currentValue, currentsuit)
 
+        'marks the new combination as true, to mark it on the display
         tracker(currentValue, currentSuit) = True
 
     End Sub
 
+    ''' <summary>
+    ''' Function to draw random number from 0 to the max value input, returns a number in that range.
+    ''' </summary>
+    ''' <param name="max"></param>
+    ''' <returns></returns>
     Function RandomNumberTo(max As Integer) As Integer
         Dim randomNumber As Integer
 
@@ -74,6 +95,12 @@ Module ShuffleTheDeck
         Return randomNumber
     End Function
 
+    ''' <summary>
+    ''' Creates the display that the user sees.
+    ''' Marks all combinations of suit and value as true, or leaves it blank.
+    ''' Marks certain values as a string to be displayed in order to show whether it is a face card, or a numbered card properly.
+    ''' </summary>
+    ''' <param name="tracker"></param>
     Sub Display(tracker(,) As Boolean)
 
         Dim temp(13, 4) As Boolean
@@ -132,7 +159,5 @@ Module ShuffleTheDeck
             Console.WriteLine()
         Next
     End Sub
-
-
 
 End Module
